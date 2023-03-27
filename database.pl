f7e30900680fc195e565611a365f3ad36f0d92b2
caden <z4h0n@ugrad.cs.ubc.ca>
2023-03-27 13:40:40 -0700

    merged two databases

--- database.pl
+++ database.pl
@@ -1,3 +1,127 @@
+% -- Year 1 courses
+course(cpsc100, 101, title, "Computational Thinking").
+course(cpsc100, 101, start, 1500).
+course(cpsc100, 101, finish, 1600).
+course(cpsc100, 101, day, "mwf").
+course(cpsc100, 101, term, 1).
+
+course(cpsc100, 201, title, "Computational Thinking").
+course(cpsc100, 201, start, 1500).
+course(cpsc100, 201, finish, 1600).
+course(cpsc100, 201, day, "mwf").
+course(cpsc100, 201, term, 1).
+
+course(cpsc103, 101, title, "Introduction to Systematic Program Design").
+course(cpsc103, 101, start, 0930).
+course(cpsc103, 101, finish, 1100).
+course(cpsc103, 101, day, "tt").
+course(cpsc103, 101, term, 1).
+
+course(cpsc103, 102, title, "Introduction to Systematic Program Design").
+course(cpsc103, 102, start, 1400).
+course(cpsc103, 102, finish, 1530).
+course(cpsc103, 102, day, "tt").
+course(cpsc103, 102, term, 1).
+
+course(cpsc103, 201, title, "Introduction to Systematic Program Design").
+course(cpsc103, 201, start, 1100).
+course(cpsc103, 201, finish, 1230).
+course(cpsc103, 201, day, "tt").
+course(cpsc103, 201, term, 2).
+
+course(cpsc103, 202, title, "Introduction to Systematic Program Design").
+course(cpsc103, 202, start, 1530).
+course(cpsc103, 202, finish, 1700).
+course(cpsc103, 202, day, "tt").
+course(cpsc103, 202, term, 2).
+
+course(cpsc107, 201, title, "Systematic Program Design").
+course(cpsc107, 201, start, 1100).
+course(cpsc107, 201, finish, 1200).
+course(cpsc107, 201, day, "mwf").
+course(cpsc107, 201, term, 2).
+
+course(cpsc110, 101, title, "Computation, Programs, and Programming").
+course(cpsc110, 101, start, 1100).
+course(cpsc110, 101, finish, 1230).
+course(cpsc110, 101, day, "tt").
+course(cpsc110, 101, term, 1).
+
+course(cpsc110, 102, title, "Computation, Programs, and Programming").
+course(cpsc110, 102, start, 1400).
+course(cpsc110, 102, finish, 1530).
+course(cpsc110, 102, day, "tt").
+course(cpsc110, 102, term, 1).
+
+course(cpsc110, 103, title, "Computation, Programs, and Programming").
+course(cpsc110, 103, start, 0930).
+course(cpsc110, 103, finish, 1100).
+course(cpsc110, 103, day, "mwf").
+course(cpsc110, 103, term, 1).
+
+course(cpsc110, 104, title, "Computation, Programs, and Programming").
+course(cpsc110, 104, start, 1230).
+course(cpsc110, 104, finish, 1400).
+course(cpsc110, 104, day, "mwf").
+course(cpsc110, 104, term, 1).
+
+course(cpsc110, 201, title, "Computation, Programs, and Programming").
+course(cpsc110, 201, start, 1230).
+course(cpsc110, 201, finish, 1400).
+course(cpsc110, 201, day, "tt").
+course(cpsc110, 201, term, 2).
+
+course(cpsc110, 203, title, "Computation, Programs, and Programming").
+course(cpsc110, 203, start, 0930).
+course(cpsc110, 203, finish, 1100).
+course(cpsc110, 203, day, "tt").
+course(cpsc110, 203, term, 2).
+
+course(cpsc121, 101, title, "Models of Computation").
+course(cpsc121, 101, start, 0930).
+course(cpsc121, 101, finish, 1100).
+course(cpsc121, 101, day, "tt").
+course(cpsc121, 101, term, 1).
+
+course(cpsc121, 102, title, "Models of Computation").
+course(cpsc121, 102, start, 1230).
+course(cpsc121, 102, finish, 1400).
+course(cpsc121, 102, day, "tt").
+course(cpsc121, 102, term, 1).
+
+course(cpsc121, 103, title, "Models of Computation").
+course(cpsc121, 103, start, 1700).
+course(cpsc121, 103, finish, 1830).
+course(cpsc121, 103, day, "tt").
+course(cpsc121, 103, term, 1).
+
+course(cpsc121, 201, title, "Models of Computation").
+course(cpsc121, 201, start, 1230).
+course(cpsc121, 201, finish, 1400).
+course(cpsc121, 201, day, "tt").
+course(cpsc121, 201, term, 2).
+
+course(cpsc121, 202, title, "Models of Computation").
+course(cpsc121, 202, start, 1530).
+course(cpsc121, 202, finish, 1700).
+course(cpsc121, 202, day, "tt").
+course(cpsc121, 202, term, 2).
+
+course(cpsc121, 203, title, "Models of Computation").
+course(cpsc121, 203, start, 1700).
+course(cpsc121, 203, finish, 1830).
+course(cpsc121, 203, day, "tt").
+course(cpsc121, 203, term, 2).
+
+course(cpsc121, 204, title, "Models of Computation").
+course(cpsc121, 204, start, 0930).
+course(cpsc121, 204, finish, 1100).
+course(cpsc121, 204, day, "tt").
+course(cpsc121, 204, term, 2).
+
+% -- Year 2 courses
+
+
 % -- Year 4 courses
 course(cpsc402, 101, title, "Numerical Linear Algebra").
 course(cpsc402, 101, start, 900).
