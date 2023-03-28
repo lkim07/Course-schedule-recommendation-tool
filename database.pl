8b3bfcb54c352fa5393ca753dd80c27363b1dc1f
lkim07 <lkim0731@gmail.com>
2023-03-27 23:23:16 -0700

    add 2nd year courses

--- database.pl
+++ database.pl
@@ -118,4 +118,112 @@ course(cpsc121, 204, start, 0930).
 course(cpsc121, 204, finish, 1100).
 course(cpsc121, 204, day, "tt").
 course(cpsc121, 204, term, 2).
+
 % --year 2
+course(cpsc203, 201, title, "Programming, Problem Solving, and Algorithms").
+course(cpsc203, 201, start, 1100).
+course(cpsc203, 201, finish, 1230).
+course(cpsc203, 201, day, "tt").
+course(cpsc203, 201, term, 2).
+
+course(cpsc210, 101, title, "Software Construction").
+course(cpsc210, 101, start, 1100).
+course(cpsc210, 101, finish, 1200).
+course(cpsc210, 101, day, "mwf").
+course(cpsc210, 101, term, 1).
+
+course(cpsc210, 102, title, "Software Construction").
+course(cpsc210, 102, start, 1300).
+course(cpsc210, 102, finish, 1400).
+course(cpsc210, 102, day, "mwf").
+course(cpsc210, 102, term, 1).
+
+course(cpsc210, 104, title, "Software Construction").
+course(cpsc210, 104, start, 1600).
+course(cpsc210, 104, finish, 1800).
+course(cpsc210, 104, day, "mwf").
+course(cpsc210, 104, term, 1).
+
+course(cpsc210, 201, title, "Software Construction").
+course(cpsc210, 201, start, 1000).
+course(cpsc210, 201, finish, 1100).
+course(cpsc210, 201, day, "mwf").
+course(cpsc210, 201, term, 2).
+
+course(cpsc210, 202, title, "Software Construction").
+course(cpsc210, 202, start, 1300).
+course(cpsc210, 202, finish, 1400).
+course(cpsc210, 202, day, "mwf").
+course(cpsc210, 202, term, 2).
+
+course(cpsc210, 203, title, "Software Construction").
+course(cpsc210, 203, start, 1500).
+course(cpsc210, 203, finish, 1600).
+course(cpsc210, 203, day, "mwf").
+course(cpsc210, 203, term, 2).
+
+course(cpsc213, 101, title, "Introduction to Computer Systems").
+course(cpsc213, 101, start, 1400).
+course(cpsc213, 101, finish, 1530).
+course(cpsc213, 101, day, "tt").
+course(cpsc213, 101, term, 1).
+
+course(cpsc213, 102, title, "Introduction to Computer Systems").
+course(cpsc213, 102, start, 1700).
+course(cpsc213, 102, finish, 1800).
+course(cpsc213, 102, day, "tt").
+course(cpsc213, 102, term, 1).
+
+course(cpsc213, 203, title, "Introduction to Computer Systems").
+course(cpsc213, 203, start, 1400).
+course(cpsc213, 203, finish, 1500).
+course(cpsc213, 203, day, "mwf").
+course(cpsc213, 203, term, 2).
+
+course(cpsc213, 204, title, "Introduction to Computer Systems").
+course(cpsc213, 204, start, 0900).
+course(cpsc213, 204, finish, 1000).
+course(cpsc213, 204, day, "mwf").
+course(cpsc213, 204, term, 2).
+
+course(cpsc213, 205, title, "Introduction to Computer Systems").
+course(cpsc213, 205, start, 1100).
+course(cpsc213, 205, finish, 1200).
+course(cpsc213, 205, day, "mwf").
+course(cpsc213, 205, term, 2).
+
+course(cpsc221, 101, title, "Basic Algorithms and Data Structures").
+course(cpsc221, 101, start, 1400).
+course(cpsc221, 101, finish, 1500).
+course(cpsc221, 101, day, "mwf").
+course(cpsc221, 101, term, 1).
+
+course(cpsc221, 102, title, "Basic Algorithms and Data Structures").
+course(cpsc221, 102, start, 1400).
+course(cpsc221, 102, finish, 1500).
+course(cpsc221, 102, day, "mwf").
+course(cpsc221, 102, term, 1).
+
+course(cpsc221, 201, title, "Basic Algorithms and Data Structures").
+course(cpsc221, 201, start, 1300).
+course(cpsc221, 201, finish, 1400).
+course(cpsc221, 201, day, "mwf").
+course(cpsc221, 201, term, 2).
+
+course(cpsc221, 202, title, "Basic Algorithms and Data Structures").
+course(cpsc221, 202, start, 1600).
+course(cpsc221, 202, finish, 1700).
+course(cpsc221, 202, day, "mwf").
+course(cpsc221, 202, term, 2).
+
+course(cpsc221, 203, title, "Basic Algorithms and Data Structures").
+course(cpsc221, 203, start, 1200).
+course(cpsc221, 203, finish, 1300).
+course(cpsc221, 203, day, "mwf").
+course(cpsc221, 203, term, 2).
+
+course(cpsc259, 101, title, "Data Structures and Algorithms for Electrical Engineers").
+course(cpsc259, 101, start, 0800).
+course(cpsc259, 101, finish, 0900).
+course(cpsc259, 101, day, "mwf").
+course(cpsc259, 101, term, 1).
