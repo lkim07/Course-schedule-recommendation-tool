709fa3a9eb970ff6bc076de982c5a96d82d0dfb1
Caden Lu <96642163+cadenlu121@users.noreply.github.com>
2023-03-28 07:23:30 -0700

    Merged 2nd and 4th year courses
    (we now need to do 3rd year courses)

--- database.pl
+++ database.pl
@@ -118,4 +118,251 @@ course(cpsc121, 204, start, 0930).
 course(cpsc121, 204, finish, 1100).
 course(cpsc121, 204, day, "tt").
 course(cpsc121, 204, term, 2).
-% --year 2
+
+% -- Year 2 courses
+course(cpsc203, 201, title, "Programming, Problem Solving, and Algorithms").
+course(cpsc203, 201, start, 1100).
+course(cpsc203, 201, finish, 1230).
+course(cpsc203, 201, day, "tt").
+course(cpsc203, 201, term, 2).
+
+course(cpsc210, 101, title, "Software Construction").
+course(cpsc210, 101, start, 1100).
+course(cpsc210, 101, finish, 1200).
+course(cpsc210, 101, day, "mwf").
+course(cpsc210, 101, term, 1).
+
+course(cpsc210, 102, title, "Software Construction").
+course(cpsc210, 102, start, 1300).
+course(cpsc210, 102, finish, 1400).
+course(cpsc210, 102, day, "mwf").
+course(cpsc210, 102, term, 1).
+
+course(cpsc210, 104, title, "Software Construction").
+course(cpsc210, 104, start, 1600).
+course(cpsc210, 104, finish, 1800).
+course(cpsc210, 104, day, "mwf").
+course(cpsc210, 104, term, 1).
+
+course(cpsc210, 201, title, "Software Construction").
+course(cpsc210, 201, start, 1000).
+course(cpsc210, 201, finish, 1100).
+course(cpsc210, 201, day, "mwf").
+course(cpsc210, 201, term, 2).
+
+course(cpsc210, 202, title, "Software Construction").
+course(cpsc210, 202, start, 1300).
+course(cpsc210, 202, finish, 1400).
+course(cpsc210, 202, day, "mwf").
+course(cpsc210, 202, term, 2).
+
+course(cpsc210, 203, title, "Software Construction").
+course(cpsc210, 203, start, 1500).
+course(cpsc210, 203, finish, 1600).
+course(cpsc210, 203, day, "mwf").
+course(cpsc210, 203, term, 2).
+
+course(cpsc213, 101, title, "Introduction to Computer Systems").
+course(cpsc213, 101, start, 1400).
+course(cpsc213, 101, finish, 1530).
+course(cpsc213, 101, day, "tt").
+course(cpsc213, 101, term, 1).
+
+course(cpsc213, 102, title, "Introduction to Computer Systems").
+course(cpsc213, 102, start, 1700).
+course(cpsc213, 102, finish, 1800).
+course(cpsc213, 102, day, "tt").
+course(cpsc213, 102, term, 1).
+
+course(cpsc213, 203, title, "Introduction to Computer Systems").
+course(cpsc213, 203, start, 1400).
+course(cpsc213, 203, finish, 1500).
+course(cpsc213, 203, day, "mwf").
+course(cpsc213, 203, term, 2).
+
+course(cpsc213, 204, title, "Introduction to Computer Systems").
+course(cpsc213, 204, start, 0900).
+course(cpsc213, 204, finish, 1000).
+course(cpsc213, 204, day, "mwf").
+course(cpsc213, 204, term, 2).
+
+course(cpsc213, 205, title, "Introduction to Computer Systems").
+course(cpsc213, 205, start, 1100).
+course(cpsc213, 205, finish, 1200).
+course(cpsc213, 205, day, "mwf").
+course(cpsc213, 205, term, 2).
+
+course(cpsc221, 101, title, "Basic Algorithms and Data Structures").
+course(cpsc221, 101, start, 1400).
+course(cpsc221, 101, finish, 1500).
+course(cpsc221, 101, day, "mwf").
+course(cpsc221, 101, term, 1).
+
+course(cpsc221, 102, title, "Basic Algorithms and Data Structures").
+course(cpsc221, 102, start, 1400).
+course(cpsc221, 102, finish, 1500).
+course(cpsc221, 102, day, "mwf").
+course(cpsc221, 102, term, 1).
+
+course(cpsc221, 201, title, "Basic Algorithms and Data Structures").
+course(cpsc221, 201, start, 1300).
+course(cpsc221, 201, finish, 1400).
+course(cpsc221, 201, day, "mwf").
+course(cpsc221, 201, term, 2).
+
+course(cpsc221, 202, title, "Basic Algorithms and Data Structures").
+course(cpsc221, 202, start, 1600).
+course(cpsc221, 202, finish, 1700).
+course(cpsc221, 202, day, "mwf").
+course(cpsc221, 202, term, 2).
+
+course(cpsc221, 203, title, "Basic Algorithms and Data Structures").
+course(cpsc221, 203, start, 1200).
+course(cpsc221, 203, finish, 1300).
+course(cpsc221, 203, day, "mwf").
+course(cpsc221, 203, term, 2).
+
+course(cpsc259, 101, title, "Data Structures and Algorithms for Electrical Engineers").
+course(cpsc259, 101, start, 0800).
+course(cpsc259, 101, finish, 0900).
+course(cpsc259, 101, day, "mwf").
+course(cpsc259, 101, term, 1).
+
+% -- Year 4 courses
+course(cpsc402, 101, title, "Numerical Linear Algebra").
+course(cpsc402, 101, start, 900).
+course(cpsc402, 101, finish, 1000).
+course(cpsc402, 101, day, "mwf").
+course(cpsc402, 101, term, 1).
+
+course(cpsc404, 101, title, "Advanced Relational Databases").
+course(cpsc404, 101, start, 1530).
+course(cpsc404, 101, finish, 1700).
+course(cpsc404, 101, day, "tt").
+course(cpsc404, 101, term, 1).
+
+course(cpsc404, 102, title, "Advanced Relational Databases").
+course(cpsc404, 102, start, 930).
+course(cpsc404, 102, finish, 1100).
+course(cpsc404, 102, day, "tt").
+course(cpsc404, 102, term, 2).
+
+course(cpsc410, 101, title, "Computational Optimization").
+course(cpsc410, 101, start, 1100).
+course(cpsc410, 101, finish, 1230).
+course(cpsc410, 101, day, "tt").
+course(cpsc410, 101, term, 2).
+
+course(cpsc411, 201, title, "Introduction to Compiler Construction").
+course(cpsc411, 201, start, 1400).
+course(cpsc411, 201, finish, 1530).
+course(cpsc411, 201, day, "tt").
+course(cpsc411, 201, term, 2).
+
+course(cpsc416, 201, title, "Distributed Systems").
+course(cpsc416, 201, start, 800).
+course(cpsc416, 201, finish, 930).
+course(cpsc416, 201, day, "tt").
+course(cpsc416, 201, term, 2).
+
+course(cpsc418, 201, title, "Parallel Computation").
+course(cpsc418, 201, start, 1500).
+course(cpsc418, 201, finish, 1600).
+course(cpsc418, 201, day, "mwf").
+course(cpsc418, 201, term, 2).
+
+course(cpsc420, 201, title, "Advanced Algorithms Design and Analysis").
+course(cpsc420, 201, start, 1300).
+course(cpsc420, 201, finish, 1400).
+course(cpsc420, 201, day, "mwf").
+course(cpsc420, 201, term, 2).
+
+course(cpsc421, 101, title, "Introduction to Theory of Computing").
+course(cpsc421, 101, start, 1230).
+course(cpsc421, 101, finish, 1400).
+course(cpsc421, 101, day, "tt").
+course(cpsc421, 101, term, 1).
+
+course(cpsc422, 101, title, "Intelligent Systems").
+course(cpsc422, 101, start, 1200).
+course(cpsc422, 101, finish, 1300).
+course(cpsc422, 101, day, "mwf").
+course(cpsc422, 101, term, 1).
+
+course(cpsc424, 101, title, "Geometric Modelling").
+course(cpsc424, 101, start, 1200).
+course(cpsc424, 101, finish, 1300).
+course(cpsc424, 101, day, "mwf").
+course(cpsc424, 101, term, 1).
+
+course(cpsc425, title, "Computer Vision").
+course(cpsc425, start, 1700).
+course(cpsc425, finish, 2000).
+course(cpsc425, day, "mwf").
+course(cpsc425, term, 1).
+
+course(cpsc425, title, "Computer Vision").
+course(cpsc425, start, 1530).
+course(cpsc425, finish, 1700).
+course(cpsc425, day, "mwf").
+course(cpsc425, term, 2).
+
+course(cpsc430, 101, title, "Computers and Society").
+course(cpsc430, 101, start, 1230).
+course(cpsc430, 101, finish, 1400).
+course(cpsc430, 101, day, "tt").
+course(cpsc430, 101, term, 1).
+
+course(cpsc430, 102, title, "Computers and Society").
+course(cpsc430, 102, start, 1400).
+course(cpsc430, 102, finish, 1530).
+course(cpsc430, 102, day, "tt").
+course(cpsc430, 102, term, 1).
+
+course(cpsc436A, 101, title, "OS DESIGN & IMPL").
+course(cpsc436A, 101, start, 900).
+course(cpsc436A, 101, finish, 1100).
+course(cpsc436A, 101, day, "tt").
+course(cpsc436A, 101, term, 1).
+
+course(cpsc436N, 101, title, "NLP").
+course(cpsc436N, 101, start, 930).
+course(cpsc436N, 101, finish, 1100).
+course(cpsc436N, 101, day, "tt").
+course(cpsc436N, 101, term, 1).
+
+course(cpsc436R, 201, title, "RANDOMIZED ALGS").
+course(cpsc436R, 201, start, 1100).
+course(cpsc436R, 201, finish, 1200).
+course(cpsc436R, 201, day, "mwf").
+course(cpsc436R, 201, term, 2).
+
+course(cpsc440, 201, title, "Advanced Machine Learning").
+course(cpsc440, 201, start, 1530).
+course(cpsc440, 201, finish, 1700).
+course(cpsc440, 201, day, "mwf").
+course(cpsc440, 201, term, 2).
+
+course(cpsc444, 201, title, "Advanced Methods for Human Computer Interaction").
+course(cpsc444, 201, start, 1530).
+course(cpsc444, 201, finish, 1730).
+course(cpsc444, 201, day, "tt").
+course(cpsc444, 201, term, 2).
+
+course(cpsc445, 201, title, "Algorithms in Bioinformatics").
+course(cpsc445, 201, start, 1530).
+course(cpsc445, 201, finish, 1700).
+course(cpsc445, 201, day, "tt").
+course(cpsc445, 201, term, 2).
+
+course(cpsc447, 201, title, "Introduction to Visualization").
+course(cpsc447, 201, start, 1400).
+course(cpsc447, 201, finish, 1700).
+course(cpsc447, 201, day, "tt").
+course(cpsc447, 201, term, 2).
+
+course(cpsc490A, 201, title, "CMP INT & DESIGN").
+course(cpsc490A, 201, start, 1730).
+course(cpsc490A, 201, finish, 1900).
+course(cpsc490A, 201, day, "mwf").
+course(cpsc490A, 201, term, 2).
