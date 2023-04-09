344179d96f907b7ac71a60c6bf3489e94e425d36
Caden Lu <96642163+cadenlu@users.noreply.github.com>
2023-04-09 11:46:02 -0700

    added suggest_courses function
    suggest_courses takes in user input to suggest a course.

--- query.pl
+++ query.pl
@@ -11,5 +11,15 @@ suggest_course(Day, Start, End, Term) :-
     fail.  % so only one course is suggested
 
 
-% NATURAL LANGUAGE PROCESSING FOR QUERYING THE DATABASE
-% TODO:
+% sequentially asks user for input (day, start time, end time, term) and calls suggest_course to suggest courses
+suggest_courses :-
+    write("What days are you looking for? Type \"mwf\" for M/W/F classes, and \"tt\" for T/T classes. "), nl,
+    read(Day),
+    write("What is the earliest start time? Give time in 24 hour format. "), nl,
+    read(Start),
+    write("What time do you want to end that class? Give time in 24 hour format. "), nl,
+    read(End),
+    write("What term are you looking for? "), nl,
+    read(Term),
+    suggest_course(Day, Start, End, Term).
+
