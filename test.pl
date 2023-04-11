e77ad64aca42fbee6ff0129a16b9e28515ded38a
lkim07 <lkim0731@gmail.com>
2023-04-10 23:21:47 -0700

    debugging

--- test.pl
+++ test.pl
@@ -9,21 +9,24 @@ packed_time_table :-
     maplist(read_n, A),
     write_list(A),
     create_array(MTWTF),
-    check_schedule(A, MTWTF).
+    check_schedule(A, MTWTF),
+    write("2"),
+    CourseSectionList = [],
+    format('The most packed course schedule is taking ~w\n', [CourseSectionList]),
+    !.
 
-%%%
-% Sum, CourseSectionList should be global variables.
-check_schedule(CourseList, Timetable):-
+check_schedule(A, MTWTF):-
+    CourseSectionList = [],
     Sum = 0,
-    CourseSectionList=[],
-    check_schedule_helper(CourseList, Timetable, CourseSectionList, Sum),
-    format('The most packed course schedule is taking ~w\n', [CourseSectionList]).
+    check_schedule_helper(A, MTWTF, CourseSectionList, Sum), !.
 
 % Recursive case: check schedule for current course and update course section list and sum
 check_schedule_helper([Course|Rest], Timetable, CourseSectionList, Sum) :-
-    NewSum =0,
-    split_string(Course, "-" , L), %
+    write("4"),
+    split_string(Course, "-" , L), 
+    write("split string"),
     L= [CourseName, Section],
+    write(L),
 
     course(CourseName, Section, term, 1),
     course(CourseName, Section, start, Start),
@@ -33,24 +36,21 @@ check_schedule_helper([Course|Rest], Timetable, CourseSectionList, Sum) :-
     StartIndex is (Start - 800) div 30,
     FinishIndex is (Finish - 800) div 30,
 
-    % fill the timetable based on the day and time
     (Day == "mwf"-> mwf(CourseName, Section, StartIndex, FinishIndex, Timetable, CourseSectionList), NewSum is Sum + (FinishIndex - StartIndex); tt(CourseName, Section, StartIndex, FinishIndex, Timetable, CourseSectionList), NewSum is Sum + (FinishIndex - StartIndex)),
 
-
-    % Check if new sum is greater than previous sum
     (NewSum > Sum ->
-        CourseSectionList = [CourseSectionList | [Course section]],
+        CourseSectionList = [CourseSectionList | [Course Section]],
         check_schedule_helper(Rest, Timetable, NewCourseSectionList, NewSum)
     ;
         check_schedule_helper(Rest, Timetable, CourseSectionList, Sum)
-    ).
+    ), !.
 
-% How to get into access[][]
 mwf(Course, Section, StartIndex, FinishIndex, Timetable, CourseSectionList):-
     % Check if there are conflicts with existing courses in the timetable
-    \+ has_conflict(Timetable[0], StartIndex, FinishIndex),
-    \+ has_conflict(Timetable[2], StartIndex, FinishIndex),
-    \+ has_conflict(Timetable[4], StartIndex, FinishIndex),
+    \+ has_conflict(Timetable, StartIndex, FinishIndex, 0),
+    \+ has_conflict(Timetable, StartIndex, FinishIndex, 2),
+    \+ has_conflict(Timetable, StartIndex, FinishIndex, 4),
+
     % Fill in the timetable with the new course
     fill_timetable(Timetable[0], StartIndex, FinishIndex),
     fill_timetable(Timetable[2], StartIndex, FinishIndex),
@@ -58,30 +58,27 @@ mwf(Course, Section, StartIndex, FinishIndex, Timetable, CourseSectionList):-
     append(CourseSectionList, [Course-Section], NewCourseSectionList).
 
 tt(Course, Section, StartIndex, FinishIndex, Timetable, CourseSectionList):-
-    \+ has_conflict(Timetable[1], StartIndex, FinishIndex),
-    \+ has_conflict(Timetable[3], StartIndex, FinishIndex),
+    \+ has_conflict(Timetable, StartIndex, FinishIndex, 1),
+    \+ has_conflict(Timetable, StartIndex, FinishIndex, 3),
+
     % Fill in the timetable with the new course
+    % col index from start to finish.
+    update_array(Timetable, 1, ColumnIndex, Value, NewMondayToFriday),  
+    update_array(Timetable, 3, ColumnIndex, Value, NewMondayToFriday),
+    % fill timetable
+    foreach(between(StartIndex, FinishIndex, Index), nth0(Index, Timetable[0], Row, [true|Row])),
+    nth0 (index, timetable[0], )
+
     fill_timetable(Timetable[1], StartIndex, FinishIndex),
     fill_timetable(Timetable[3], StartIndex, FinishIndex),
     append(CourseSectionList, [Course-Section], NewCourseSectionList).
 
 % Predicate to check if there are conflicts with existing courses in the timetable
-has_conflict(Timetable, StartIndex, FinishIndex) :-
+has_conflict(Timetable, StartIndex, FinishIndex, RowIndex) :-
+    nth0(RowIndex, Timetable, Row)
     between(StartIndex, FinishIndex, Index),
-    nth0(Index, Timetable, Row),
-    member(true, Row).
-
-% Predicate to fill timetable with new course
-fill_timetable(Timetable, StartIndex, FinishIndex) :-
-    % Fill in cells between start and finish indices with true
-    foreach(between(StartIndex, FinishIndex, Index), nth0(Index, Timetable, Row, [true|Row])).
-
-% Predicate to undo filling of timetable with new course
-undo_fill_timetable(Timetable, StartIndex, FinishIndex) :-
-    % Fill in cells between start and finish indices with false
-    foreach(between(StartIndex, FinishIndex, Index), nth0(Index, Timetable, Row, [false|Row])).
-
-%%%%
+    nth0(Index, Row, Rows),
+    member(true, Rows).
 
 read_n(X) :-
     write('Enter courses and their section (ex: cpsc100-101): '),
